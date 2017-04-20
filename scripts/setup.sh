7848c3be8a60d5dade283e746b129ac5377718b8
James S. White <jameswhite@github.com>
2017-04-20 15:01:26 -0500

    init

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
-
-DOTFILES=$HOME/dotfiles
-
-for f in `ls -A $DOTFILES/home`; do
-  #ln -sf $DOTFILES/home/$f $HOME/ 
-  echo $f
-done
-
-if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-  rm -f $HOME/.gvimrc
-  rm -f $HOME/.vimrc
-  rm -rf $HOME/.vim
-
-  cp $DOTFILES/home/.gvimrc $HOME/.gvimrc 
-  cp $DOTFILES/home/.vimrc $HOME/.vimrc 
-  cp -r $DOTFILES/home/.vim $HOME/.gvimrc 
-fi
+$(dirname $0)/../home/bin/dotmanaged -i
